45493dab22bd389949abf201ee92c68d39b62c72
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-08-14 00:08:34 +0300

    Mongoose Added

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const server = express();
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config();
 
 const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to mongoDB");
+  } catch (err) {
+    throw err;
+  }
+};
 
 server.listen(PORT, () => {
+  connect();
   console.log(`Server is running on port ${PORT}`);
 });
