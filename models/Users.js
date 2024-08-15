fc1c6baf82f5d8dd07f01c27017ad992db2d04b1
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-08-15 22:20:43 +0300

    Users Schema Added

---
+++ models/Users.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const usersSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    surname: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
