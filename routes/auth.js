26c606a0c346c5fd21e6074815fc501524a15a30
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-08-30 21:49:41 +0300

    Comment Lines Added

--- routes/auth.js
+++ routes/auth.js
@@ -5,14 +5,22 @@ const bcrypt = require("bcrypt");
 //POST Register
 api.post("/register", async (req, res) => {
   try {
+    //HTTP Post isteğinde body'den gönderilen veriler
     const { name, surname, email, password } = req.body;
+
+    //Bcrypt Hash oluşturmak için
     const salt = await bcrypt.genSalt(10);
+
+    //password değerini oluşturulan hash ile şifrelemek için
     const hashedPassword = await bcrypt.hash(password, salt);
+
     const existingUser = await Users.findOne({ email });
+
+    //Kullanıcı Varsa Kontrolü
     if (existingUser) {
       return res.status(403).json({ error: "Email already in use!" });
     }
   } catch (err) {
-    console.log(err);
+    res.status(500).json(err);
   }
 });
