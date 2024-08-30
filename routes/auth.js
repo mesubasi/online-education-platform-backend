dfcb5b06e1775cb41d18d06a9dfe67f5042e9673
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-08-30 22:03:07 +0300

    New Control Added and Middleware preparations

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const api = require("express").Router();
 const bcrypt = require("bcrypt");
 
 //POST Register
-api.post("/register", async (req, res) => {
+const handleNewUser = async (req, res) => {
   try {
     //HTTP Post isteğinde body'den gönderilen veriler
     const { name, surname, email, password } = req.body;
@@ -20,7 +20,25 @@ api.post("/register", async (req, res) => {
     if (existingUser) {
       return res.status(403).json({ error: "Email already in use!" });
     }
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and Password Are Required!",
+      });
+    }
+
+    //Veritabanına body'den gelen değerleri yakaldıktan sonra kaydetmek için
+    const newUser = newUser({
+      name,
+      surname,
+      email,
+      password,
+    });
+    await newUser.save();
+
+    //Kullanıcı Başarıyla Kayıt Olduğunda HTTP 200 Kodu Dönder.
+    res.status(200).json("A New User Created Successfully.");
   } catch (err) {
     res.status(500).json(err);
   }
-});
+};
