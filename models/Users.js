eee7f25d956120fde8ee945cfb59e963da11b9c0
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-08-30 22:25:53 +0300

    Security for Database

--- models/Users.js
+++ models/Users.js
@@ -3,10 +3,37 @@ const { Schema } = mongoose;
 
 const usersSchema = new Schema(
   {
-    name: { type: String, required: true },
-    surname: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
+    name: { type: String, required: true, unique: true, trim: true },
+    surname: { type: String, required: true, unique: true, trim: true },
+    email: {
+      type: String,
+      required: [true, "Email is required!"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: [
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+        "Please provide a valid email address",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is Required!"],
+      minlength: [6, "Password must be at least 6 characters long"],
+      validate: {
+        validator: function (value) {
+          // Özel Doğrulama Mantığı
+          return (
+            /[A-Z]/.test(value) && // At least one uppercase letter
+            /[a-z]/.test(value) && // At least one lowercase letter
+            /[0-9]/.test(value) && // At least one digit
+            /[!@#$%^&*]/.test(value) // At least one special character
+          );
+        },
+        message:
+          "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character from !@#$%^&*",
+      },
+    },
   },
   {
     timestamps: true,
