0f6a010febe70dc8637d0f3157722575cf6fcc07
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-08-31 12:51:29 +0300

    ValidPassword Control Added

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -2,6 +2,7 @@ const Users = require("../models/Users");
 const bcrypt = require("bcrypt");
 const rateLimit = require("express-rate-limit");
 const path = require("path");
+const jwt = require("jsonwebtoken");
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, //15 Dakika Bekleme
@@ -22,7 +23,7 @@ const handleLogin = async (req, res) => {
       return res.status(404).json({ error: "User Not Found!" });
     }
 
-    //Body'den gelen parolayı ve email adresiyle eşleşen kullanıcının parolasını karşılaştır.
+    //Body'den gelen parolayı ve email adresiyle eşleşen kullanıcının parolasını karşılaştır
     const validPassword = await bcrypt.compare(
       req.body.password,
       user.password
@@ -33,12 +34,18 @@ const handleLogin = async (req, res) => {
       return res.status(403).json({ error: "Invalid Password!" });
     }
 
-    return res.status(200).json({
-      user: user._id,
-      name: user.name,
-      surname: user.surname,
-      email: user.email,
-    });
+    //Parola doğru girilmişe değerleri dönder
+    if (validPassword) {
+      const accessToken = jwt.sign({ email: user.email }, null, {
+        expiresIn: "30s",
+      });
+      return res.status(200).json({
+        user: user._id,
+        name: user.name,
+        surname: user.surname,
+        email: user.email,
+      });
+    }
   } catch (err) {
     res.status(500).json({ error: "Login failed", details: err.message });
   }
