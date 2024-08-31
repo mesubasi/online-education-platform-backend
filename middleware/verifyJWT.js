938a5767d62233288ed662706d64e7e729558513
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-08-31 13:35:34 +0300

    verifyJWT Edits

---
+++ middleware/verifyJWT.js
@@ -0,0 +1,14 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  if (!authHeader) return res.sendStatus(401);
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) return res.sendStatus(403);
+    req.user = decoded.username;
+    next();
+  });
+};
+
+module.exports = verifyToken;
