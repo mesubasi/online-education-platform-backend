1a8d806320e1c27583d7a6edb49b21dbcf66630b
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-09-30 21:16:29 +0300

    course post name changed

--- routes/course.js
+++ routes/course.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const courseController = require("../controllers/courseController");
 
-router.post("/", courseController.handleCourse);
+router.post("/add-course", courseController.handleCourse);
 
 module.exports = router;
