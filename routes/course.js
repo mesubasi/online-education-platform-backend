800006effe98f79343c4c8a1ea7909eb5a8090e4
Muhammed Emin Subaşı <mesub08@gmail.com>
2024-10-13 22:48:29 +0300

    Edits for API

--- routes/course.js
+++ routes/course.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const courseController = require("../controllers/courseController");
 
-router.post("/courses", courseController.postCourse);
-router.get("/courses", courseController.getCourse);
+router.post("/courses/post", courseController.postCourse);
+router.get("/courses/get", courseController.getCourse);
 
 module.exports = router;
